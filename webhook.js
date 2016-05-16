123be7d3149cef6fd15079f6d06efcaa44097670
Matt <mattwebdev@users.noreply.github.com>
2016-05-16 16:17:29 -0700

    Create webhook.js

---
+++ webhook.js
@@ -0,0 +1,6 @@
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
